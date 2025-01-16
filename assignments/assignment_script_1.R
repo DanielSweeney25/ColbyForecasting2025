d4c39da91c98c6db3cc845b3046575a56f6f2ad2
Bloom <bloom@B1109.local>
2025-01-16 11:05:19 -0500

    attempted to make webpage using quarto unsuccessfully. completed assignment 1 using demo data and produced demo output plot

--- assignments/assignment_script_1.R
+++ assignments/assignment_script_1.R
@@ -3,5 +3,26 @@ buoys = gom_buoys()
 coast = read_coastline()
 
 #filtering each dataset
-m01 <- bouys %>%
-  dplyr::filter(id == "static")
+m01 <- buoys %>%
+  dplyr::filter(id == "M01")
+
+RCP45 <- DB %>%
+  dplyr::filter(scenario == "RCP45",
+                year == "2055",
+                interval == "mon")
+
+#read in covariates
+covars = read_brickman(db)
+
+#extract varaibles from covariates
+extraction = extract_brickman(covars, buoys, form = "wide")
+
+#order the months chronologically
+extraction = extraction %>%
+  mutate(month = factor(month, levels = month.abb))
+
+#plot the sst by month for buouy m01 in scenario rcp45
+ggplot(extraction, aes(x=month, y=SST))+
+  geom_point()+
+  theme_bw()+
+  labs(x="Month", y="Sea Surface Temperature (C)", title = "Predicted Sea Surface Temperature at Buouy M01 in 2055 in Scenario RCP45")
